522f165d11d88036fb82eec1e7008ff1c6d4b37b
Deracodes1 <ezemeniachidera@gmail.com>
2026-04-03 05:33:11 -0700

    added swagger docs for proper api documenation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './global/transform.interceptor';
 import { AllExceptionsFilter } from './global/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -19,6 +20,16 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+  const config = new DocumentBuilder()
+    .setTitle('Yetty Rental APi')
+    .setDescription(
+      'THE OFFICIAL DOCUMENTATION FOR YETTY RENTALS: This api powers the equipment rentals/management service.',
+    )
+    .setVersion('1.0')
+    .addTag('yetty')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
